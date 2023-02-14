d661a0494b8782e6bfe0f61107472c0a146e58e0
SamTruj <elitemorado123@hotmail.com>
2023-02-13 19:14:17 -0600

    all new

---
+++ intro-java/index.js
@@ -0,0 +1,14 @@
+/*
+document.write('Escribiendo desde Js')
+alert('Bienvenido a mi primer sitio con Js')
+*/
+
+document.write('Bienvenido ', prompt('Ingresa tu nombre '))
+
+var nombreGlogal = 'Miguel'
+
+function pruebaNombre(){
+    let nombreLocal = 'Edgardo'
+}
+
+pruebaNombre()
